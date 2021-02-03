959c918deae9d6f53d10bd1538a478ea67c10987
AbhishekP1999* <abhishek1999.29@gmail.com>
2021-02-04 02:30:57 +0530

    reverse an array

---
+++ reverse_array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0; i<n; i++){
+			cin>>arr[i];
+		}
+		for(int i=0, j=n-1; i<j; i++, j--){ 
+			swap(arr[i], arr[j]);
+		}
+		for(int i=0; i<n; i++){
+			cout<<arr[i]<<" ";
+		}
+	}
+	return 0;
+}
