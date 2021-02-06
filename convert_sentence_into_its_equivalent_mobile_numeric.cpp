de5444e0fba9c92981d6cbd4c0ecac451be236b8
AbhishekP1999* <abhishek1999.29@gmail.com>
2021-02-06 23:20:17 +0530

    sentence to mobile equivalent sequence

---
+++ convert_sentence_into_its_equivalent_mobile_numeric.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+string printSequence(string arr[], 
+					string input) 
+{ 
+	string output = ""; 
+
+	int n = input.length(); 
+	for (int i=0; i<n; i++) 
+	{ 
+		if (input[i] == ' ') 
+			output = output + "0"; 
+
+		else
+		{ 
+			int position = input[i]-'A'; 
+			output = output + arr[position]; 
+		} 
+	} 
+
+	return output; 
+} 
+
+int main() 
+{ 
+	string str[] = {"2","22","222", 
+			"3","33","333", 
+			"4","44","444", 
+			"5","55","555", 
+			"6","66","666", 
+			"7","77","777","7777", 
+			"8","88","888", 
+			"9","99","999","9999"
+				}; 
+
+	string input = "HELLOWORLD"; 
+	cout << printSequence(str, input); 
+	return 0; 
+} 
