cc1c44aaf0c5daa5508ae82ee520bd10091ba85b
AbhishekP1999* <abhishek1999.29@gmail.com>
2021-02-09 15:20:09 +0530

    computer not get by customer

---
+++ find_no_of_computer_not_get_by_a_customer.cpp
@@ -0,0 +1,49 @@
+#include<iostream> 
+#include<cstring> 
+using namespace std; 
+
+#define MAX_CHAR 26 
+
+int runCustomerSimulation(int n, const char *seq) 
+{ 
+	char seen[MAX_CHAR] = {0}; 
+
+	int res = 0; 
+
+	int occupied = 0; 
+
+	for (int i=0; seq[i]; i++) 
+	{ 
+		int ind = seq[i] - 'A'; 
+
+		if (seen[ind] == 0) 
+		{ 
+			seen[ind] = 1; 
+
+			if (occupied < n) 
+			{ 
+				occupied++; 
+
+				seen[ind] = 2; 
+			} 
+
+			else
+				res++; 
+		} 
+
+		else
+		{ 
+		if (seen[ind] == 2) 
+			occupied--; 
+		seen[ind] = 0; 
+		} 
+	} 
+	return res; 
+} 
+
+int main() 
+{ 
+	cout << runCustomerSimulation(2, "ABBAJJKZKZ") << endl; 
+	cout << runCustomerSimulation(1, "ABCBCA") << endl; 
+	return 0; 
+} 
